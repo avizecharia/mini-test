1fddba77f16062a6077ea3793c32f13de9fbf4bb
aviel <aviel469@gmail.com>
2024-10-31 12:53:31 +0200

    cretae get list func

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Add from "./components/Add";
 import Grid from "./components/Grid";
+import Mission from "./models/Mission";
 
 export default function App() {
-  const [missionList, setmissionList] = useState("");
+  const [missionList, setMissionList] = useState<Mission[]>([]);
+  useEffect(() => {
+    const data = async () => {
+      const res = await fetch(
+        "https://reactexambackend.onrender.com/missions/8797742"
+      );
+      const resJson = await res.json();
+      setMissionList([...resJson]);
+    };
+    data()
+    
+  }, [missionList]);
   return (
-    <div>
-      <Add />
-      <Grid />
+    <div className="app">
+      <h1>Milatry OperationDashboard</h1>
+      <Add setMissionList={setMissionList} />
+      <Grid missionList={missionList} />
     </div>
   );
 }

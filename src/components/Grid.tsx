037b22619cca710c6e40d064b698868419b0b295
aviel <aviel469@gmail.com>
2024-10-31 12:53:57 +0200

    create read and done

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import Mission from "../models/Mission";
+import MissionCard from "./MissionCard";
 
-export default function Grid() {
-  return <div>Grid</div>;
+interface Props {
+    missionList:Mission[]
+}
+
+export default function Grid({missionList}:Props) {
+
+  return (
+    <div className="grid">
+        {missionList.length == 0 && <h1>Your list is Empty</h1>}
+        { 
+        missionList.map(mission => <MissionCard mission={mission}/>)
+        }
+    </div>
+  )
 }
